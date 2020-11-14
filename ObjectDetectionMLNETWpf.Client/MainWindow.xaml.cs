92fa623cb4fd586e843039541315edda4f02c76c
Sam <dsaeresearch@gmail.com>
2020-11-14 12:54:46 -0500

    Added web cam viewing with Emgu.CV.

--- ObjectDetectionMLNETWpf.Client/MainWindow.xaml.cs
+++ ObjectDetectionMLNETWpf.Client/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,14 +18,69 @@ using System.Windows.Shapes;
 
 namespace ObjectDetectionMLNETWpf.Client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        VideoCapture m_capture = new VideoCapture();
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            dispatcherTimer.Start();
+
+            m_capture = new VideoCapture();
         }
+
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            using (Image<Bgr, byte> nextFrame = m_capture.QueryFrame().ToImage<Bgr, Byte>())
+            {
+                if (nextFrame != null)
+                {
+                    Image<Gray, byte> grayframe = nextFrame.Convert<Gray, byte>();
+
+                    //   var faces = m_cascade.DetectMultiScale(grayframe, 1.1, 3, new System.Drawing.Size(20, 20));
+
+                    //foreach (var face in faces)
+                    //{
+                    //    nextFrame.Draw(face, new Bgr(0, 0, 0), 3);
+                    //}
+
+                    TestImage1.Source = ToBitmapSource(nextFrame);
+                    //  TestImage2.Source = ToBitmapSource(nextFrame);
+
+                }
+            }
+        }
+
+
+
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        public static BitmapSource ToBitmapSource(Image<Bgr, Byte> image)
+        {
+            using (System.Drawing.Bitmap source = image.ToBitmap())
+            {
+                IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap  
+
+                BitmapSource bs = System.Windows.Interop
+                  .Imaging.CreateBitmapSourceFromHBitmap(
+                  ptr,
+                  IntPtr.Zero,
+                  Int32Rect.Empty,
+                  System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                DeleteObject(ptr); //release the HBitmap  
+                return bs;
+            }
+        }
+
     }
 }
---
+++ documentation.md
@@ -0,0 +1,8 @@
+steps in creating project
+-create solution and project for WPF 
+-manage nuget project
+	-Emgu.CV (4.4.0.4099)
+	-Emgu.CV.runtime.windows (4.4.0.4099)
+	-Emgu.CV.Bitmap (4.4.0.4099)
+
+
